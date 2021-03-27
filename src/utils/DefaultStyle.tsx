303ccdff9071df47a223210b58b65d47998a8d80
seungyeop Kim <seungyeop2002@gmail.com>
2021-03-27 17:37:59 +0900

    Add DefaultStyle

---
+++ src/utils/DefaultStyle.tsx
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const DefaultSetting = createGlobalStyle`
+  body {
+    margin: 0px;
+    padding: 0px;
+  }
+  div {
+    box-sizing: border-box;
+  }
+`;
+
+export default DefaultSetting;
